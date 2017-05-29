21af353c0c6efc1ae7c2cbdceaac48fdc64085fe
Dem Pilafian <dem@centerkey.com>
2017-05-29 00:31:49 -0700

    Upgrade to jsdom 11

--- spec.js
+++ spec.js
@@ -19,20 +19,20 @@ const html = `
 </html>
 `;
 
-const document = require('jsdom').jsdom(html);
-const window =   document.defaultView;
-const $ =        require('jquery')(window);
-const dna =      require('dna.js')(window, $);
-const app =      require('./app.js')(window, $, dna);
-const spec =     require('tape');
-const colorize = require('tap-spec');
+const { JSDOM } = require('jsdom');
+const window =    new JSDOM(html).window;
+const $ =         require('jquery')(window);
+const dna =       require('dna.js')(window, $);
+const app =       require('./app.js')(window, $, dna);
+const spec =      require('tape');
+const colorize =  require('tap-spec');
 spec.createStream().pipe(colorize()).pipe(process.stdout);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 spec('Utility function dna.array.fromMap()', (assert) => {
    const does = 'converts a map into an array of maps';
    const map = { a: { word: 'Ant' }, b: { word: 'Bat' } };
-   const actual = dna.array.fromMap(map, 'letter');
+   const actual =   dna.array.fromMap(map, 'letter');
    const expected = [{ word: 'Ant', letter: 'a' }, { word: 'Bat', letter: 'b' }];
    assert.deepEqual(actual, expected, does);
    assert.end();
@@ -42,7 +42,7 @@ spec('Utility function dna.array.fromMap()', (assert) => {
 spec('Lunch', (assert) => {
    const does = 'is ording and eating bulgogi';
    app.doLunch();
-   const actual = dna.getModel('task');
+   const actual =   dna.getModel('task');
    const expected = [{ title: 'Order bulgogi' }, { title: 'Eat bulgogi' }];
    assert.deepEqual(actual, expected, does);
    assert.end();
